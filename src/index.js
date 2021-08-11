91057a042493bd668ee1c82c2353887dacd822b9
sha-pasha <sh.pasha.v@gmail.com>
2021-08-11 03:09:37 +0300

    edit burger-menu

--- src/index.js
+++ src/index.js
@@ -45,5 +45,6 @@ const menu = document.querySelector(".menu");
 
 burgerMenu.addEventListener("click", () => {
   menu.classList.toggle("open");
+  burgerMenu.firstElementChild.classList.toggle("open");
 });
 
